99c44e86fb2775846c402ace76ea8646a00fbf64
ggarcia23 <gagarcia3@bsu.edu>
2022-11-29 17:35:54 -0500

    added method which allows insertion of ais data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
+import pymongo
+import json
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017")
+myDataBase = myClient["AISTestData"]
+myCollection = myDataBase["aisdk_20201118"]
+
+
+class TrafficMonitoringBackEnd:
+
+    def insert_ais(ais_data):
+        json_object = json.dumps(ais_data)
+        with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+
+        with open("sample.json") as file:
+            file_data = json.load(file)
+
+        myCollection.insert_one(file_data)
+
+        return print("Number of Insertions: ")
 
 
 def main():
-    print("hello world");
+    print("hello world")
 
 
 if __name__ == '__main__':
--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,15 @@
 import unittest
+import pymongo
+import main
+
+myClient = pymongo.MongoClient("mongodb://localhost:27017")
+myDataBase = myClient["AISTestData"]
+myCollection = myDataBase["aisdk_20201118"]
+
+test_ais = {"Timestamp": "2020-11-18T00:00:00.000Z", "Class": "Class A", "MMSI": 210169000, "MsgType": "static_data",
+            "IMO": 9584865, "CallSign": "5BNZ3", "Name": "KATHARINA SCHEPERS", "VesselType": "Cargo",
+            "CargoTye": "Category X", "Length": 152, "Breadth": 24, "Draught": 7.8, "Destination": "NODRM",
+            "ETA": "2020-11-18T09:00:00.000Z", "A": 143, "B": 9, "C": 13, "D": 11}
 
 
 class TestStringMethods(unittest.TestCase):
@@ -17,3 +28,7 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
+    def test_insert_ais(self):
+        x = main.TrafficMonitoringBackEnd
+        x.insert_ais(test_ais)
+        self.assertTrue(myCollection.find({"IMO": 9584865}))
